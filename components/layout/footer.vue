3b552b2152c78f33eb86edc44b71cffe44e62d16
giovannilaperna <giovannilaperna@gmail.com>
2023-03-18 15:09:52 +0100

    add footer

---
+++ components/layout/footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer class="footer">
+    <div class="level container">
+      <div class="level-left">
+      </div>
+      <div class="level-right">
+        <div class="level-item">
+          <div class="content">
+            <div class="buttons">
+              <nuxt-link
+                v-for="locale in availableLocales"
+                :key="locale.code"
+                :to="switchLocalePath(locale.code)"
+                class="button is-text"
+              >
+              {{ locale.flag }} {{ locale.name }}
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.is-text {
+  text-decoration: none;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,41 @@
 <template>
-  <nuxt />
+  <div class="is-tall">
+    <main class="is-tall-container">
+      <nuxt />
+    </main>
+    <div v-if="showChevron" class="is-hidden-mobile is-bottom-right">
+      <b-tooltip :label="$t('scrollToTop')" position="is-left">
+        <b-icon icon="chevron-up" type="is-black" size="is-medium" @click.native="scrollToTop"></b-icon>
+      </b-tooltip>
+    </div>
+    <layout-footer />
+  </div>
 </template>
 
 <script>
+import layoutFooter from '~/components/layout/footer'
 export default {
-  name: 'DefaultLayout'
+  components: {
+    layoutFooter
+  },
+  data: () => ({
+    showChevron: false
+  }),
+  mounted() {
+    setInterval(() => {
+      this.showChevron = (window.pageYOffset > 0) ? true : false
+    }, 1000)
+  }
 }
 </script>
+
+<style>
+.is-tall {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+.is-tall-container {
+  flex: 1;
+}
+</style>
