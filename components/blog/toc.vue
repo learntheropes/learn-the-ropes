191d945e97b92e4a8293f2198500289ae13552a7
giovannilaperna <giovannilaperna@gmail.com>
2023-03-18 16:50:54 +0100

    remove unused columns

--- components/blog/toc.vue
+++ components/blog/toc.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="columns">
-      <div class="column">
-      <div class="box">
-        <div class="title is-5">{{ $t('toc') }}</div>
-        <ul>
-          <li v-for="section of toc" :key="section.id">
-            <b-button tag="nuxt-link" :to="`#${section.id}`" class="is-text">{{ section.text }}</b-button>
-          </li>
-        </ul>
-      </div>
-    </div>
+  <div class="box">
+    <div class="title is-5">{{ $t('toc') }}</div>
+    <ul>
+      <li v-for="section of toc" :key="section.id">
+        <b-button tag="nuxt-link" :to="`#${section.id}`" class="is-text">{{ section.text }}</b-button>
+      </li>
+    </ul>
   </div>
 </template>
 
