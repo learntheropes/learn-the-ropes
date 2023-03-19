40df8d5d4995755479c29838d40838284f606a12
giovannilaperna <giovannilaperna@gmail.com>
2023-03-19 17:13:55 +0100

    remove unused div

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,29 +1,27 @@
 <template>
-  <div>
-    <div class="container">
-      <section class="section">
-        <h1 class="title is-1 has-text-primary">
-          {{ post.title }}
-        </h1>
-        <div class="subtitle is-5">
-          {{ post.description }}
-        </div>
-        <div class="block is-italic">
-          {{$t('published')}} {{ $moment(post.createdAt).fromNow() }} 
-          <span v-if="isUpdated">
-            <br class="is-hidden-tablet">
-            <span class="is-hidden-mobile"> | </span>
-            {{$t('updated')}} {{ $moment(post.updatedAt).fromNow() }}
-        </span>
-        </div>
-      </section>
-      <blog-toc :toc="post.toc" />
-      <section class="section">
-        <div class="content">
-          <nuxt-content :document="post" />
-        </div>
-      </section>
-    </div>
+  <div class="container">
+    <section class="section">
+      <h1 class="title is-1 has-text-primary">
+        {{ post.title }}
+      </h1>
+      <div class="subtitle is-5">
+        {{ post.description }}
+      </div>
+      <div class="block is-italic">
+        {{$t('published')}} {{ $moment(post.createdAt).fromNow() }} 
+        <span v-if="isUpdated">
+          <br class="is-hidden-tablet">
+          <span class="is-hidden-mobile"> | </span>
+          {{$t('updated')}} {{ $moment(post.updatedAt).fromNow() }}
+      </span>
+      </div>
+    </section>
+    <blog-toc :toc="post.toc" />
+    <section class="section">
+      <div class="content">
+        <nuxt-content :document="post" />
+      </div>
+    </section>
   </div>
 </template>
 
