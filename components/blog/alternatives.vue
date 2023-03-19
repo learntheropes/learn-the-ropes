ab63bc04810ec906b151f53737836fbd731fe984
giovannilaperna <giovannilaperna@gmail.com>
2023-03-20 00:04:15 +0100

    fix teample

--- components/blog/alternatives.vue
+++ components/blog/alternatives.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="section">
+  <section class="section content">
     <i18n path="alt" tag="div">
       <template v-slot:lang>
         <p v-for="alternative of alternatives" :key="alternative.path">
-          <nuxt-link :to="alternative.path">{{ language(alternative.path) }} </nuxt-link>
+          <nuxt-link :to="alternative.path">{{ language(alternative.path) }}</nuxt-link>
         </p>   
       </template>
     </i18n> 
